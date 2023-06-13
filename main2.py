f52845753d7c99bff04af404888d88c323ac44f2
Dharani-Gurumoorthy <120043186+Dharani-Gurumoorthy@users.noreply.github.com>
2023-06-13 17:40:44 +0530

    Update main2.py
    The provided Python code sends a promotional email with a customized title, message, and an embedded image. It utilizes the smtplib module to connect to an SMTP server and sends the email using Gmail's SMTP server. The email content is created using HTML, and the image is attached to the email as an embedded image. The code prompts the user for input to customize the title, message, and recipient email address. Once the email is sent successfully, a success message is displayed. This code is a convenient way to send personalized promotional emails with attractive images.

--- main2.py
+++ main2.py
@@ -4,8 +4,8 @@ from email.mime.text import MIMEText
 
 def send_email(title, message, recipient):
     # Email configuration
-    sender_email = 'gdharanids@gmail.com'  # Replace with your Gmail address
-    sender_password = 'ksnmtayiunsjqcwj'  # Replace with your Gmail password
+    sender_email = 'xyz@gmail.com'  # Replace with your Gmail address
+    sender_password = 'abcde'  # Replace with your Gmail password
 
     # Create the email message
     email_message = MIMEMultipart('alternative')
